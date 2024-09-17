03dea3f1a46841a4d602da3138f7e2f7c0b013e7
Dawn3901 <15530012682@163.com>
2024-09-17 17:27:11 +0800

    Create function&static.cpp

---
+++ function&static.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+void swap(int* a, int* b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void Swap(int& a, int& b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void func(int& a) {
+	a += 80;
+}
+
+void log(const char* message) {
+	cout << message << endl;
+}
+
+void Print_i() {
+	static int i = 1;//只在第一次调用时声明和赋值，生存周期是全程，但只能在函数内使用
+	cout << i++ << endl;
+}
+
+void Print_j() {
+	int j = 1;//每次调用会声明和赋值，且调用完就销毁
+	cout << j++ << endl;
+}
+
+int k=1;//全局变量，生存周期全程，且在任何位置可以使用
+void Print_k() {
+	cout << k ++<< endl;
+}
+
+int main()
+{
+	Print_i();
+	Print_i();
+	Print_i();
+	Print_i();
+	Print_i();
+
+	Print_j();
+	Print_j();
+	Print_j();
+	Print_j();
+	Print_j();
+
+	Print_k();
+	Print_k();
+	Print_k();
+	Print_k();
+	Print_k();
+
+	return 0;
+}
