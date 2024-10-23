e65f597f6bfb2aa14dde11fd762c575a8337e651
张晓行 <15530012682@163.com>
2024-10-23 19:51:35 +0800

    Create test.cpp

---
+++ leetcode题解/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void RemoveNthFromEnd_19();
+void IsVaild_20();
+void MergeList_21();
+void ReverseKGroup_25();
+
+int main()
+{
+    ReverseKGroup_25();
+    return 0;
+}
